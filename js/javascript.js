c4bfcce2be8f97a01f71e7bcf427362e4761ca45
plondon <plondon@gm.slc.edu>
2014-03-21 15:24:46 -0400

    add up arrow functionality

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
+	//hamburger
 	$('#hamburger').on("click", function(){
 		$('#hamburger-menu').toggleClass('active-header');
 		$('#wrapper').toggleClass('active-header');
 	});
+	//second nav-bar page navigation
 	$('.second-nav ul a').on("click", function(e) {
 		var sti_position = $($(this).attr('href')).offset().top-180
 		console.log(sti_position)
@@ -11,4 +13,9 @@ $(document).ready(function(){
 	        scrollTop: sti_position
 			    }, 'slow');
 	});	
+	$('.up-arrow').on("click", function(){
+		$("html, body").animate({
+			scrollTop: $('#wrapper').offset().top-181
+		}, 'slow');
+	});
 })
