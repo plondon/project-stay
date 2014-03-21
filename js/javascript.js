634c0537ff9b520594efb601d1366eff390cb85b
plondon <plondon@gm.slc.edu>
2014-03-21 17:33:59 -0400

    add functionality to second header

--- js/javascript.js
+++ js/javascript.js
@@ -4,6 +4,17 @@ $(document).ready(function(){
 		$('#hamburger-menu').toggleClass('active-header');
 		$('#wrapper').toggleClass('active-header');
 	});
+
+	// second nav-bar reveal effect
+	$('#bar .panel a').on("mouseover", function(){
+		var second_nav_id = $(this).attr('id')
+		$('.second-nav').removeClass('active-second-header')
+		$('#' + second_nav_id + '.second-nav').addClass('active-second-header')
+	});
+	$('.second-nav').on("mouseleave", function(){
+		$('.second-nav').removeClass('active-second-header')
+	});
+
 	//second nav-bar page navigation
 	$('.second-nav ul a').on("click", function(e) {
 		var sti_position = $($(this).attr('href')).offset().top-180
